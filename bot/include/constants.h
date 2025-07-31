7f4b595b10da5ad5d80982bd81329be7a192e25d
uwo-o <hugo.campos@usm.cl>
2025-07-30 21:53:48 -0400

    updates

--- bot/include/constants.h
+++ bot/include/constants.h
@@ -4,16 +4,21 @@
 #define WIFI_SSID "movistar2,4GHZ_AD9286"
 #define WIFI_PWS "hCU7cnG9ghJP"
 #define DEVELOPMENT true
+
 const int BUZZER_PIN = 5;
+
 const int MPU_6050_BUS = 0;
 const int MPU_6050_DIR = 0x68;
 const int MPU_6050_SDA = 26;
 const int MPU_6050_SCL = 27;
+
 const int GPS_BAUDRATE = 9600;
+
 const int BMP_280_BUS = 1;
 const int BMP_280_SDA = 21;
 const int BMP_280_SCL = 22;
 const int BMP_280_DIR = 0x76;
+
 const int SERVO_FREQ = 60;
 const int SERVO_MIN = 125;
 const int SERVO_MAX = 575;
@@ -22,7 +27,7 @@ const int SERVO_SDA = 12;
 const int SERVO_SCL = 13;
 const int SERVO_DIR = 0x40;
 
-// Motors
+// Movement constants
 const int TOP_RIGHT_PIN = 35;
 const int TOP_LEFT_PIN = 22;
 const int BOTTOM_RIGHT_PIN = 34;
